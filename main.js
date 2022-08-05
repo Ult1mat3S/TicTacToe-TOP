e0a29fbbee4e5c8da13a8c70e0987639e7763a68
UItimate <52617311+UItimate@users.noreply.github.com>
2022-08-05 02:42:35 -0600

    feat: Display X and O's on the page

---
+++ main.js
@@ -0,0 +1,31 @@
+const gameBoard = (() => {
+    board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+    return board;
+})
+
+const displayController = (() => {
+    let container = document.querySelector('#container');
+    // let displayBoard = document.createElement('h1')
+    // displayBoard.setAttribute('id', 'board');
+
+    for (i = 0; i < board.length; i++) {
+        // let itemsContainer = document.createElement('h1')
+        // itemsContainer.setAttribute('id', 'itemsContainer');
+        // let selectItemsContainer = document.querySelector('#itemsContainer');
+        let boardItems = document.createElement('p')
+        boardItems.textContent = `${board[i]}`
+        let board1 = document.createElement('div')
+        let selectBoard = document.querySelector('div');
+        container.appendChild(boardItems)
+    }
+})
+
+function players() {
+    let player1 = null
+    let player2 = null
+}
+
+
+gameBoard()
+displayController()
+
